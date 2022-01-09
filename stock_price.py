66c99ad30bd9b11d274e22d6eee19d8c65a3bad8
Guido <guidosassaroli@gmail.com>
2022-01-09 20:57:40 +0100

    first commit

--- stock_price.py
+++ stock_price.py
@@ -20,7 +20,7 @@ data = web.DataReader(company, 'yahoo', start, end)
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
 
-prediction_days = 60 
+prediction_days = 60
 
 x_train = []
 y_train = []
